97f4ebc8fa92cc1ba5283b8dd03da3d5a68d1c93
pcingame <80826643+pcingame@users.noreply.github.com>
2023-01-04 19:46:45 +0700

    :)

--- app/src/main/java/com/example/playcoin/presentation/coin/CoinActivity.kt
+++ app/src/main/java/com/example/playcoin/presentation/coin/CoinActivity.kt
@@ -16,41 +16,41 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class CoinActivity : AppCompatActivity() {
     private var valueRepeat = 3
-    private lateinit var binding : ActivityCoinBinding
-    private val viewModel : CoinViewModel by viewModels()
-    private var id:String = ""
+    private lateinit var binding: ActivityCoinBinding
+    private val viewModel: CoinViewModel by viewModels()
+    private var id: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCoinBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        if(intent!=null){
+        if (intent != null) {
             val id = intent.getStringExtra("id")
-            Log.d("insideIntent",id.toString())
+            Log.d("insideIntent", id.toString())
             viewModel.getCoinById("tether")
             callCoinApi(id.toString())
         }
     }
 
-    private fun callCoinApi(id:String){
+    private fun callCoinApi(id: String) {
         CoroutineScope(Dispatchers.Main).launch {
-            repeat(valueRepeat){
-                Log.d("id",id)
-                viewModel._coinValue.collect{value->
-                    Log.d("id",id)
+            repeat(valueRepeat) {
+                Log.d("id", id)
+                viewModel._coinValue.collect { value ->
+                    Log.d("id", id)
                     when {
                         value.isLoading -> {
                             binding.coinProgressBar.visibility = View.VISIBLE
-                            Log.d("idloading",id)
+                            Log.d("idloading", id)
                         }
                         value.error.isNotBlank() -> {
                             binding.coinProgressBar.visibility = View.GONE
-                            Log.d("iderror",id)
+                            Log.d("iderror", id)
                             valueRepeat = 0
                         }
-                        value.coinDetail!=null->{
-                            Log.d("id",id)
-                            Log.d("coindetail",value.coinDetail.name)
+                        value.coinDetail != null -> {
+                            Log.d("id", id)
+                            Log.d("coindetail", value.coinDetail.name)
                             binding.coinProgressBar.visibility = View.GONE
                             valueRepeat = 0
                             Picasso.get().load(value.coinDetail.image).into(binding.imgCoinDetail)
@@ -58,8 +58,10 @@ class CoinActivity : AppCompatActivity() {
                             binding.txtCoinPrice.text = value.coinDetail.price.toString()
                             binding.txtCoinPriceLow.text = value.coinDetail.low_price.toString()
                             binding.txtCoinPriceHigh.text = value.coinDetail.high_price.toString()
-                            binding.txtCoinMarketCap.text = value.coinDetail.price_percentage_change.toString()
-                            binding.txtCoinPricePercentChange.text = value.coinDetail.price_percentage_change.toString()
+                            binding.txtCoinMarketCap.text =
+                                value.coinDetail.price_percentage_change.toString()
+                            binding.txtCoinPricePercentChange.text =
+                                value.coinDetail.price_percentage_change.toString()
                         }
                     }
                 }
@@ -67,10 +69,11 @@ class CoinActivity : AppCompatActivity() {
             }
         }
     }
+
     override fun onStart() {
         super.onStart()
-        if(id.isBlank()){
-            if(intent!=null){
+        if (id.isBlank()) {
+            if (intent != null) {
                 id = intent.getStringExtra("id").toString()
                 viewModel.getCoinById(id)
                 callCoinApi(id)
